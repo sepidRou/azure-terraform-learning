8ef2f02ccbb36eaeca147c6ad51fa2804502b24f
Sepi <sepideh.rouhani66@gmail.com>
2025-06-02 17:51:35 +0200

    week04 done

---
+++ week04/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-week04"
+  location = "westeurope"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "week04storageacct"        
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "example-container"
+  storage_account_name = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
