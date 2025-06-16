30bcd53ee5c38758d1d1aa173f55c6307cd49263
Sepi <sepideh.rouhani66@gmail.com>
2025-06-16 15:40:11 +0200

    week03

--- week03/main.tf
+++ week03/main.tf
@@ -1,3 +1,5 @@
+// week03 - Azure Storage Account and Blob Container
+
 terraform {
   required_providers {
     azurerm = {
@@ -12,6 +14,20 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "myResourceGroup"
+  name     = "week04ResourceGroup"
   location = "westeurope"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "week04storageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
 }
